3471bea2893578cb89535eeacb410a443b90c744
Chang King <cking@example.com>
2020-09-17 20:56:49 -0400

    add stages

--- vars/pipe_func.groovy
+++ vars/pipe_func.groovy
@@ -1,22 +1,24 @@
 def call(Map pipelineParams) {
   pipeline {
     agent any
-    
-    stages ('deploy dev'){
-      steps {
-        echo "deploying to ${pipelineParams.devServer} port ${pipelineParams.devPort}"
+   
+    stages {
+      stage ('deploy dev'){
+        steps {
+          echo "deploying to ${pipelineParams.devServer} port ${pipelineParams.devPort}"
+        }
       }
-    }
-
-    stages ('deploy uat'){
-      steps {
-        echo "deploying to ${pipelineParams.uatServer} port ${pipelineParams.uatPort}"
+  
+      stage ('deploy uat'){
+        steps {
+          echo "deploying to ${pipelineParams.uatServer} port ${pipelineParams.uatPort}"
+        }
       }
-    }
-
-    stages ('deploy prod'){
-      steps {
-        echo "deploying to ${pipelineParams.prodServer} port ${pipelineParams.prodPort}"
+  
+      stage ('deploy prod'){
+        steps {
+          echo "deploying to ${pipelineParams.prodServer} port ${pipelineParams.prodPort}"
+        }
       }
     }
   }
