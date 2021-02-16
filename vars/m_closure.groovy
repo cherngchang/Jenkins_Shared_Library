bc53186741611e2f64821779cdbac9e549d163ee
Chang King <cking@example.com>
2021-02-15 21:12:20 -0500

    test

---
+++ vars/m_closure.groovy
@@ -0,0 +1,70 @@
+def call(body) {
+  // evaluate the body block, and collect configuration into the object
+  def pipelineParams= [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = pipelineParams
+  body()
+
+  //def LOG_LEVEL = '0'
+
+  pipeline {
+    agent {
+      kubernetes {
+        cloud "${pipelineParams.my_cloud}"
+	label "Test-${pipelineParams.my_cloud}"
+	yaml """\
+        apiVersion: v1
+        kind: Pod
+        metadata:
+          labels:
+            some-label: some-label-value
+        spec:
+          containers:
+          - name: maven
+            image: maven:alpine
+            command:
+            - cat
+            tty: true
+	    volumeMounts:
+	    - name: vol
+	      mountPath: "/build"
+	  - name: gitter
+	    image: alpine/git
+	    command:
+	    - cat
+	    tty: true
+	    volumeMounts:
+            - name: vol
+              mountPath: "/build"
+	  volumes:
+	  - name: vol
+	    emptyDir: {}
+        """.stripIndent()
+      }
+    }
+    
+    stages {
+      stage ('Check out Repo') {
+        steps {
+          container('gitter') {
+	    dir ("/build") {
+              script {
+                git url: "${pipelineParams.git_url}"
+	      }
+	    }
+	  }
+        }
+      }
+
+      stage ('Run Build'){
+        steps {
+	  container('maven') {
+            dir ("/build") {
+              sh "mvn clean install"
+	    }
+	  }
+        }
+      }
+    }
+  }
+}
