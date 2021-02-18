8cc3df0a6cc50318eae6c4835fbf1285b8de7e51
Chang King <cking@example.com>
2021-02-17 23:17:59 -0500

    new stage in m_closure.groovy

--- vars/m_closure.groovy
+++ vars/m_closure.groovy
@@ -11,34 +11,64 @@ def call(body) {
     agent {
       kubernetes {
         cloud "${pipelineParams.my_cloud}"
-	//label "Test-${pipelineParams.my_cloud}"
+	label "TestSharedLib"
+	idleMinutes 5
 	yaml """\
         apiVersion: v1
         kind: Pod
-        metadata:
-          labels:
-            some-label: some-label-value
         spec:
           containers:
           - name: maven
             image: changking/maven
             command:
             - cat
-	    tty: true
+	  - name: docker
+	    image: docker:18.06.1
+	    command: ["tail", "-f", "/dev/null"]
+	    volumeMounts:
+	      - name: docker
+	        mountPath: /var/run/docker.sock # We use the k8s host docker engine
+	  volumes:
+	  - name: docker
+            hostPath:
+	      path: /var/run/docker.sock
         """.stripIndent()
+	defaultContainer 'maven'
       }
     }
     
     stages {
+      stage ('Checkout Application Repo') {
+        steps {
+          container('maven') {
+            script {
+              git url: "${pipelineParams.git_url}"
+	    }
+	  }
+	}
+      }
+
       stage ('Run Build'){
         steps {
 	  container('maven') {
-            sh "mvn -version"
+            sh "mvn clean package"
 	  }
         }
       }
-    }
-    post {
+
+      stage('Build Docker Image') {
+        steps {
+          container('docker') {
+            script {
+              docker.withRegistry( '', 'dockerhub' ) { //dockerhub is the credential id
+                def dockerImage = docker.build("changking/testsharedlib:dev")
+                dockerImage.push()
+              }
+            }
+	  }
+	}
+      }
+   /* post {
       success {
         container('jnlp') {
 	  script {
@@ -46,7 +76,7 @@ def call(body) {
 	    echo "Success Build"
 	  }
 	}
-      }
+      }*/
     }
   }
 }
